df91f8d49121bf2bbd36e68ffa27c9c8f031f73a
Aditya Kumar <adityakumar@MacBook-Air.local>
2020-06-12 16:06:53 +0530

    Removed chrome extension

--- src/Components/App.js
+++ src/Components/App.js
@@ -11,37 +11,37 @@ import SpecificCountry from "./SpecificCountry"
 import { BrowserRouter, Route } from "react-router-dom"
 
 class App extends React.Component {
-	renderLoading = () => {
-		if (this.props.storeCoronaData.lastUpdate) {
-			return (
-				<div>
-					<Route path="/" component={Home} exact />
-					<Route component={Data} path="/data" exact />
-					<Route component={SpecificCountry} path="/data/:name" />
-				</div>
-			)
-		} else {
-			return <Loading />
-		}
-	}
+  renderLoading = () => {
+    if (this.props.storeCoronaData.lastUpdate) {
+      return (
+        <div>
+          <Route path="/" component={Home} exact />
+          <Route component={Data} path="/data" exact />
+          <Route component={SpecificCountry} path="/data/:name" />
+        </div>
+      )
+    } else {
+      return <Loading />
+    }
+  }
 
-	componentDidMount() {
-		this.props.getCoronaData()
-		this.props.getCountryList()
-	}
-	render() {
-		return (
-			<BrowserRouter>
-				<NavigationBar />
-				{this.renderLoading()}
-			</BrowserRouter>
-		)
-	}
+  componentDidMount() {
+    this.props.getCoronaData()
+    this.props.getCountryList()
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <NavigationBar />
+        {this.renderLoading()}
+      </BrowserRouter>
+    )
+  }
 }
 var mapStateToProps = (state) => {
-	return state
+  return state
 }
 export default connect(mapStateToProps, {
-	getCoronaData: getCoronaData,
-	getCountryList: getCountryList,
+  getCoronaData: getCoronaData,
+  getCountryList: getCountryList,
 })(App)
--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,12 +2,6 @@ import { createStore, compose, applyMiddleware } from "redux"
 import allReucers from "./Reducers"
 import thunk from "redux-thunk"
 
-var myStore = createStore(
-	allReucers,
-	compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
-)
+var myStore = createStore(allReucers, compose(applyMiddleware(thunk)))
 
 export default myStore
